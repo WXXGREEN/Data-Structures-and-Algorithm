25cb7a9b993267153587ff2ee01617491df28543
WXXGREEN <1141195807@qq.com>
2016-03-01 11:04:10 +0800

    second commit

--- LinkStack.cpp
+++ LinkStack.cpp
@@ -1,48 +1,46 @@
-//栈的链式存储及其算法实现
-//by zoe
-
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef int Elemtype;
 typedef struct node{
-  Elemtype elem;
-  struct node *next;
+ Elemtype elem;
+ struct node *next;
 }*top;
 int m=sizeof(struct node);
 
 struct node *Push(struct node *top,Elemtype x){
-  struct node *p;
-  p=(struct node *)malloc(m);
-  p->next=top;
-  top=p;
-  p->elem=x;
-  return top;
+ struct node *p;
+ p=(struct node *)malloc(m);
+ p->next=top;
+ top=p;
+ p->elem=x;
+ return top;
 }
 
 struct node *Pop(struct node *top,Elemtype &x){
-  if(top==NULL) return 0;
-  struct node *p;
-  p=top;
-  x=p->elem;
-  top=top->next;
-  delete p;
-  return top;
+ if(top==NULL) return 0;
+ struct node *p;
+ p=top;
+ x=p->elem;
+ top=top->next;
+ delete p;
+ return top;
 }
 
 Elemtype GetTop(struct node *top){
-  if(top==NULL) return -1;
-  int x=top->elem;
-  return x;
+ if(top==NULL) return -1;
+ int x=top->elem;
+ return x;
 }
 
 int main(){
-  struct node *p;
-  int x=0,y=0;
-  Push(p,2);
-  Push(p,34);
-  Pop(p,x);
-  printf("%d",x);
-  y=GetTop(p);
-  return 0;
+ struct node *top;
+ int x=0,y=0;
+ top=Push(top,2);
+ top=Push(top,34);
+ top=Pop(top,x);
+ printf("%d\n",x);
+ y=GetTop(top);
+ printf("%d\n",y);
+ return 0;
 }
